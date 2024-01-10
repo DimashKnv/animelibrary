6280c82f020cc40b6b646c6e5388cac0bc78379e
Dimash Knv <kunakpayevd@gmail.com>
2024-01-10 20:04:10 +0600

    contacts_page_upd_styled

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,11 @@
 export function Contacts(){
-    return <div className="Contacts_main">
+    return <div className="contacts_main">
         <h1>Contact Me</h1>
         <h2>If you have any questions or suggestions, feel free to reach out to me</h2>
     
         <form action="">
-            <input type="text" placeholder="Your name" />
-            <input type="text" placeholder="Your Email"/>
+            <input type="text" placeholder=" Your name" />
+            <input type="text" placeholder=" Your Email"/>
             <textarea name="" id="" cols="30" rows="10" placeholder="Commentary"></textarea>
             <button>Submit</button>
             </form>
